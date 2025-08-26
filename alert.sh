da923100a9f24ef73da08fa9f63058625c033339
gill <gill@redhat.com>
2025-08-26 18:29:32 +0530

    updated alert.sh

--- alert.sh
+++ alert.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <koala_ID>"
+  exit 1
+fi
 
-ssh pagill@kproxy -tt sshto $1 -tt /bin/bash << 'EOT'
-show-scheduled -c
+KOALA_ID="$1"
+
+ssh pagill@kproxy -tt "sshto $KOALA_ID -tt bash << 'INNER'
 virsh list --all
-EOT
-
-#alias sp=ssh pagill@kproxy
-#ssh pagill@kproxy -tt "sshto 2127 -tt 'show-scheduled -c && whoami'"
+ssh examserver \"
+show-phase-state -av
+# Print bold yellow text for visibility before next command
+echo -e '\\033[1;33mBelow is the current state of the exam setup\\033[0m'
+show-exam-state
+\"
+INNER"
